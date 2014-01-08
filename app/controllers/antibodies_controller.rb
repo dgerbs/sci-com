d8de9835fc1ab0f5ef50c0e87a9f4f2b7df51578
dgerbs <dojoh@outlook.com>
2014-01-08 16:28:57 -0500

    Complete Antibody resource

--- app/controllers/antibodies_controller.rb
+++ app/controllers/antibodies_controller.rb
@@ -1,2 +1,56 @@
 class AntibodiesController < ApplicationController
+  before_filter :authenticate_user!, except: [:index]
+
+  def index
+    @antibodies = Antibody.all
+  end
+
+  def show
+    @antibody = Antibody.find(params[:id])
+  end
+
+  def new
+    @antibody = Antibody.new
+  end
+
+  def create
+    @antibody = Antibody.new(antibody_params)
+    if @antibody.save
+      redirect_to @antibody, notice: 'Antibody successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    if user_signed_in?
+      @antibody = Antibody.find(params[:id])
+    else
+      render :edit
+    end
+  end
+
+  def update
+    @antibody = Antibody.find(params[:id])
+    if @antibody.update(antibody_params)
+      redirect_to @antibody, notice: 'Antibody successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @antibody = Antibody.find(params[:id])
+    if @antibody.destroy
+      redirect_to @antibody
+    else
+      render @antibody
+    end
+  end
+
+  protected
+  
+    def antibody_params
+      params.require(:antibody).permit(:target, :antibody, :vendor, :product, :isotype)
+    end
 end
