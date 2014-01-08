58650ae20d6a6cc79603b97ec0b350a2db59506c
dgerbs <dojoh@outlook.com>
2014-01-08 16:42:02 -0500

    Add model validation

--- app/models/antibody.rb
+++ app/models/antibody.rb
@@ -1,2 +1,8 @@
 class Antibody < ActiveRecord::Base
+
+  validates_presence_of :product
+  validates_presence_of :antibody
+  validates_presence_of :target
+  validates_presence_of :vendor
+  validates_presence_of :isotype
 end
