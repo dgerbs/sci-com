083db0daa57c4921a472385754188c66189a7631
dgerbs <dojoh@outlook.com>
2014-01-18 23:34:38 -0500

    Modify user index action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,6 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @users }
-    end
   end
 
   def show
