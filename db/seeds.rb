6584e11bf487bb8deebf3564361f2c62332188e2
dgerbs <dojoh@outlook.com>
2014-02-06 13:20:01 -0500

    Complete db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+
+CSV.foreach(Rails.root.join("db/antibodymaster.csv"), headers: true) do |row|
+
+  antibodies = {
+    target: row[0].gsub(/\,/,"").gsub(/\$/,""),
+    antibody: row[1].gsub(/\,/,"").gsub(/\$/,""),
+    vendor: row[2].gsub(/\,/,"").gsub(/\$/,""),
+    product: row[3].gsub(/\,/,"").gsub(/\$/,""),
+    isotype: row[4].gsub(/\,/,"").gsub(/\$/,"")
+  }
+
+  if Antibody.where(antibodies).empty?
+    Antibody.create(antibodies) 
+  end
+end
