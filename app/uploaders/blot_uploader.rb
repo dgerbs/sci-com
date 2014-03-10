e73bb49252a87ed88b9e6a4a3dd99a0a9453c3c0
dgerbs <dojoh@outlook.com>
2014-03-10 13:43:59 -0400

    Add default blot

--- app/uploaders/blot_uploader.rb
+++ app/uploaders/blot_uploader.rb
@@ -22,12 +22,9 @@ class BlotUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url
+    ActionController::Base.helpers.asset_path("assets/" + [version_name, "default.png"].compact.join('_'))
+  end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
@@ -37,10 +34,15 @@ class BlotUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-   version :blot do
-     process :resize_to_fill => [630, 400]
-     process convert: 'png'
-   end
+  version :blot do
+    process :resize_to_fill => [630, 400]
+    process convert: 'png'
+  end
+
+  # version :protocolblot do
+  #   process resize_to_fill: [630, 400]
+  # end
+
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
