b082d903d82ee70c752d02028002bdf4016fb98e
dgerbs <dojoh@outlook.com>
2014-03-10 15:31:03 -0400

    Fix strong params call

--- app/controllers/ifprotocols_controller.rb
+++ app/controllers/ifprotocols_controller.rb
@@ -44,7 +44,7 @@ class IfprotocolsController < ApplicationController
     @ifprotocol = current_user.ifprotocols.find(params[:id])
 
     respond_to do |format|
-      if @ifprotocol.update_attributes(ib_params)
+      if @ifprotocol.update_attributes(if_params)
         format.html { redirect_to @ifprotocol }
         format.js
       else
