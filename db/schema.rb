0af9cbf12992cd3a0c2d33806bafe36d55a971ac
dgerbs <dojoh@outlook.com>
2014-03-10 15:44:10 -0400

    Add blot to ipreprotocols

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140310193245) do
+ActiveRecord::Schema.define(version: 20140310193956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -159,6 +159,7 @@ ActiveRecord::Schema.define(version: 20140310193245) do
     t.integer  "antibody_id",           null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "blot"
   end
 
   add_index "ipreprotocols", ["user_id", "antibody_id", "created_at"], name: "index_ipreprotocols_on_user_id_and_antibody_id_and_created_at", using: :btree
