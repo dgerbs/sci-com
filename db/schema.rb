0f118ee0d43ed9941e55b4eba5600fe4532be2ac
dgerbs <dojoh@outlook.com>
2014-03-10 12:19:33 -0400

    Add blot uploader

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140225134702) do
+ActiveRecord::Schema.define(version: 20140310155526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -127,6 +127,7 @@ ActiveRecord::Schema.define(version: 20140225134702) do
     t.integer  "antibody_id",         null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "blot"
   end
 
   add_index "ihcprotocols", ["user_id", "antibody_id", "created_at"], name: "index_ihcprotocols_on_user_id_and_antibody_id_and_created_at", using: :btree
